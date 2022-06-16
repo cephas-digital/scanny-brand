dbd544679193095e5d570fce44f97adc5a80eb1d
Ayokayzy <theayokayzy1@gmail.com>
2022-06-16 06:18:37 -1100

    javascript functionality

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-// background color
-const bgPicker = document.getElementById("bg-picker");
-const bgHex = document.getElementById("bg-hex");
-const phone = document.querySelector(".phone")
+// explore btn
+const exploreBtn = document.getElementById("explore-btn");
+const exploreClose = document.querySelector("#explore-close")
+// explore modal
+const exploreModal = document.querySelector("#explore-modal");
+
+exploreBtn.addEventListener("click", () => {
+    exploreModal.classList.toggle("explore-modal")
+});
+exploreClose.addEventListener("click", () => {
+    exploreModal.classList.toggle("explore-modal")
+    console.log("working");
+});
+// exploreModal.addEventListener("click", () => {
+//     exploreModal.classList.toggle("explore-modal")
+// });
+
+// sidenav-profile
+const sidenavProfile = document.querySelector(".profile")
+const sidenavProfileModal = document.querySelector(".profile-modal")
+sidenavProfile.addEventListener("mouseover", () => {
+    sidenavProfileModal.classList.remove("show-profile-modal")
+})
+sidenavProfile.addEventListener("mouseout", () => {
+    sidenavProfileModal.classList.add("show-profile-modal")
+})
+
+// mobile menu
+
+const menuBar = document.querySelector(".menu-bar")
+const menuModal = document.querySelector(".menu-modal")
+menuBar.addEventListener("click", () => {
+    menuModal.classList.toggle("show-nav")
+})
+
 // button color 
 // button font color
 // font color
 
-bgPicker.addEventListener("input", () => {
-    const bgValue = bgPicker.value;
-    phone.style.backgroundColor = bgValue;
-    bgHex.value = bgValue;
-})
-bgHex.addEventListener("input", () => {
-    const bgValue = bgHex.value;
-    phone.style.backgroundColor = bgValue;
-    bgPicker.value = bgValue;
-})
+
